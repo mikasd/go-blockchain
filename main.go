a563121dd4f7ff189fdcb203b3d9c33bf63a7d25
mikasd2 <michael.ingley@gmail.com>
2020-10-16 23:14:00 -0700

    sets up basic blockchain methods and tests them by making len4 blockchain

--- main.go
+++ main.go
@@ -1,11 +1,56 @@
 package main
 
 import (
+	"bytes"
+	"crypto/sha256"
 	"fmt"
-	"rsc.io/quote"
 )
 
-func main() {
-	fmt.Println(quote.Hello())
+type BlockChain struct {
+	blocks []*Block
 }
 
+type Block struct {
+	Hash     []byte
+	Data     []byte
+	PrevHash []byte
+}
+
+func (b *Block) DeriveHash() {
+	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
+	hash := sha256.Sum256(info)
+	b.Hash = hash[:]
+}
+
+func CreateBlock(data string, prevHash []byte) *Block {
+	block := &Block{[]byte{}, []byte(data), prevHash}
+	block.DeriveHash()
+	return block
+}
+
+func (chain *BlockChain) AddBlock(data string) {
+	prevBlock := chain.blocks[len(chain.blocks)-1]
+	new := CreateBlock(data, prevBlock.Hash)
+	chain.blocks = append(chain.blocks, new)
+}
+
+func Genesis() *Block {
+	return CreateBlock("origin", []byte{})
+}
+
+func InitBlockChain() *BlockChain {
+	return &BlockChain{[]*Block{Genesis()}}
+}
+
+func main() {
+	chain := InitBlockChain()
+	chain.AddBlock("second block")
+	chain.AddBlock("third block")
+	chain.AddBlock("fourth block")
+
+	for _, block := range chain.blocks {
+		fmt.Printf("Prev Hash: %x\n", block.PrevHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Current Hash: %x\n", block.Hash)
+	}
+}
